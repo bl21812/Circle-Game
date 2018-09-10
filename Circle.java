2154225b7ac91db58cceda37cfa49858f3732601
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-09-10 11:25:02 -0400

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,14 +1,29 @@
-class Circle extends Shape {
 
-  double radius;
+public class Circle extends Shape {
 
-  public Circle(double radius, double x, double y) {
-    super(x, y);
-    this.radius = radius;
-  }
-  
-  public double getArea() {
-    return ((Math.pow(radius,2)) * Math.PI));
-  }
+	private double radius;
 
+    public Circle(double radius, double x, double y) {
+    	super(x, y);
+	    this.radius = radius;
+	    setBoundingBoxSize((int)(Math.round(radius*2)), (int)(Math.round(radius*2)));
+    }
+	  
+    public double getArea() {
+    	return ((Math.pow(radius,2)) * Math.PI);
+    }
+    
+    public void setRadius(double radius) {
+    	this.radius = radius;
+    	setBoundingBoxSize((int)(Math.round(radius*2)), (int)(Math.round(radius*2)));
+    }
+    
+    public double getRadius() {
+    	return radius;
+    }
+    
+    private void setBoundingBoxSize(int width, int height) {
+    	super.getBoundingBox().setSize(width, height);
+    }
+    
 }
