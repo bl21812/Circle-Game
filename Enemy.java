cbe02cce6f88e8222eb1538d622ab2b82f356008
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-09-10 11:26:52 -0400

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,24 @@
+
+public class Enemy extends Square implements Moveable {
+
+	Enemy(double height, double width, double x, double y) {
+		super(height, width, x, y);
+	}
+	
+	public void moveUp() {
+		
+	}
+	
+	public void moveDown() {
+		
+	}
+
+	public void moveRight() {
+	
+	}
+
+	public void moveLeft() {
+	
+	}
+	
+}
